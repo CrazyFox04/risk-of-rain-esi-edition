d14c8b7363b4922de9ed233f380b40f7029fcc8d
Enzo Renard <60904@etu.he2b.be>
2024-11-08 17:18:02 +0100

    Round screen refresh rate to int

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -18,7 +18,7 @@ public class SettingsMenu : MonoBehaviour
         int currentResolutionIndex = 0;
         
         for (int i = 0; i < resolutions.Length; i++) {
-            string option = resolutions[i].width + " x " + resolutions[i].height + " " + resolutions[i].refreshRateRatio + "Hz";
+            string option = resolutions[i].width + " x " + resolutions[i].height + " " + Mathf.RoundToInt((float)resolutions[i].refreshRateRatio.value) + "Hz";
             
             options.Add(option);
             
