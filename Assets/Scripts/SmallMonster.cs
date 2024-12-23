486cc74b0150805ff446e0db10936f1203eee009
Marcel Angielczyk <99039274+Ixarito@users.noreply.github.com>
2024-12-23 18:09:06 +0100

    ressources

--- Assets/Scripts/SmallMonster.cs
+++ Assets/Scripts/SmallMonster.cs
@@ -6,22 +6,7 @@ using UnityEngine.Serialization;
 public class SmallMonster : AbstractEnemy
 {
     //-----------------Enemy Movement-----------------
-    
-    public void jump()
-    {
-        if (isGrounded)
-        {
-            rb.AddForce(new Vector2(0f, jumpForce));
-        }
-    }
-    
-    //-----------------Enemy On Collision Behaviors-----------------
-    
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        jump();
-    }
-    
+
     //-----------------Enemy Animation-----------------
     
 }
