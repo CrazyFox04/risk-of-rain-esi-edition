d77071f0732c9159780f3cf0c692ee5004cc1f41
Enzo Renard <60904@etu.he2b.be>
2024-12-25 10:49:02 +0100

    Add get_area_guid func & sync cpp submodule

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -28,6 +28,9 @@ public class GameController : MonoBehaviour
     [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
     private static extern void takePlayerDamage(IntPtr game, int damage);
     
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern int get_area_guid_current_level(IntPtr game, int x, int y)
+    
     static GameController()
     {
         Debug.Log("Plugin name: " + dllname);
@@ -61,5 +64,10 @@ public class GameController : MonoBehaviour
     void takePlayerDamage(int damage)
     {
         takePlayerDamage(this.game, damage);
+    }
+
+    int get_area_guid_current_level(int x, int y)
+    {
+        return get_area_guid_current_level(this.game, x, y);
     }
 }
