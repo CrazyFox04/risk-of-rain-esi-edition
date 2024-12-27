1f325983892ade2eb3a6b7f2c24e60c2a1581072
Enzo Renard <60904@etu.he2b.be>
2024-12-27 21:26:36 +0100

    Fix gameController.cs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.InteropServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
@@ -21,262 +22,252 @@ public class GameController : MonoBehaviour
     
     [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
     private static extern void takePlayerDamage(IntPtr game, int damage);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern int getAreaGuidCurrentLevel(IntPtr game, int x, int y);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern int ifCanSpawnCurrentLevelSpawnAt(IntPtr game, int areaX, int areaY, int spawnId);
     
     [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern int getAreaGuidCurrentLevel(IntPtr game, int x, int y)
+    private static extern string getType(IntPtr game, int id);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern int getCharacterSpeed(IntPtr game, int id);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern double getCharacterJumpForce(IntPtr game, int id);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern double getEnemyFollowRange(IntPtr game, int id);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern double getEnemyAttackRange(IntPtr game, int id);
     
     [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern int ifCanSpawnCurrentLevelSpawnAt(IntPtr game, int areaX, int areaY, int spawnId)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern const char* getType(Intptr game, int id)
+    private static extern bool canCharacterAttack(IntPtr game, int id, string attackName);
         
     [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern int getCharacterSpeed(IntPtr game, int id)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern double getCharacterJumpForce(IntPtr game, int id)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern double getEnemyFollowRange(IntPtr game, int id)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern double getEnemyAttackRange(IntPtr game, int id)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern bool canCharacterAttack(IntPtr game, int id, const char* attackName)
+    private static extern double getDamage(IntPtr game, int id, string attackName);
         
     [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern double getDamage(IntPtr game, int id, const char* attackName)
+    private static extern double getChargeTime(IntPtr game, int id, string attackName);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern double getCharacterHurtTime(IntPtr game, int id);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern bool isCharacterBusy(IntPtr game, int id);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern int getCharacterHealth(IntPtr game, int id);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern int getCharacterMaxHealth(IntPtr game, int id);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern double getPlayerDashForce(IntPtr game);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern double getJetPackForce(IntPtr game);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern double getJetPackMaxTime(IntPtr game);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern double getPlayerLandingTime(IntPtr game);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern double getPlayerDashTime(IntPtr game);
+    
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern double getCharacterAttackTime(IntPtr game, int id, string attackName);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern bool isPlayerDashing(IntPtr game);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern bool isPlayerUsingJetpack(IntPtr game);
+    
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern bool canCharacterMove(IntPtr game, int id, string movementName);
         
     [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern double getChargeTime(IntPtr game, int id, const char* attackName)
-        
+    private static extern double getCharacterCoolDownAttack(IntPtr game, int id, string attackName);
+
     [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern double getCharacterHurtTime(IntPtr game, int id)
+    private static extern bool isAValidId(IntPtr game, int id);
+
+    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+    private static extern int getPlayerId(IntPtr game);
     
     [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern bool isCharacterBusy(IntPtr game, int id)
+    private static extern void attack(int id, string attackName, int targetId);
     
     [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern int getCharacterHealth(IntPtr game, int id)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern int getCharacterMaxHealth(IntPtr game, int id)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern double getPlayerDashForce(IntPtr game)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern double getJetPackForce(IntPtr game)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern double getJetPackMaxTime(IntPtr game)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern double getPlayerLandingTime(IntPtr game)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern double getPlayerDashTime(IntPtr game)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern double getCharacterAttackTime(IntPtr game, int id, const char* attackName)
-        
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern bool isPlayerDashing(IntPtr game)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern bool isPlayerUsingJetpack(IntPtr game)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern bool canCharacterMove(IntPtr game, int id, const char* movementName)
-        
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern double getCharacterCoolDownAttack(IntPtr game, int id, const char* attackName)
-        
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern bool isAValidId(IntPtr game, int id)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern int getPlayerId(IntPtr game)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern void attack(int id, const char* attackName, int targetId)
-    
-    [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
-    private static extern void move(IntPtr game, int id, const char* movementName)
+    private static extern void move(IntPtr game, int id, string movementName);
         
     static GameController()
     {
         Debug.Log("Plugin name: " + dllname);
     }
 
-    void Start ()
+    public void Start ()
     {
         this.game = newGame();
     }
 
-    int GetPlayerMaxHealth()
+    public int GetPlayerMaxHealth()
     {
         return getPlayerMaxHealth(this.game);
     }
 
-    int GetPlayerCurrentHealth()
+    public int GetPlayerCurrentHealth()
     {
         return getPlayerCurrentHealth(this.game);
     }
 
-    void AddPlayerMaxHealth(int health)
-    {
-        addPlayerMaxHealth(this.game, health);
-    }
-
-    void AddPlayerHealth(int health)
-    {
-        addPlayerHealth(this.game, health);
-    }
-
-    void TakePlayerDamage(int damage)
+    public void TakePlayerDamage(int damage)
     {
         takePlayerDamage(this.game, damage);
     }
 
-    int GetAreaGuidCurrentLevel(int x, int y)
+    public int GetAreaGuidCurrentLevel(int x, int y)
     {
         return getAreaGuidCurrentLevel(this.game, x, y);
     }
     
-    int IfCanSpawnCurrentLevelSpawnAt(int areaX, int areaY, int spawnId)
+    public int IfCanSpawnCurrentLevelSpawnAt(int areaX, int areaY, int spawnId)
     {
         return ifCanSpawnCurrentLevelSpawnAt(this.game, areaX, areaY, spawnId);
     }
     
-    const char* GetType(int id)
+    public string GetType(int id)
     {
         return getType(this.game, id);
     }
     
-    int GetCharacterSpeed(int id)
+    public int GetCharacterSpeed(int id)
     {
         return getCharacterSpeed(this.game, id);
     }
     
-    double GetCharacterJumpForce(int id)
+    public double GetCharacterJumpForce(int id)
     {
         return getCharacterJumpForce(this.game, id);
     }
     
-    double GetEnemyFollowRange(int id)
+    public double GetEnemyFollowRange(int id)
     {
         return getEnemyFollowRange(this.game, id);
     }
     
-    double GetEnemyAttackRange(int id)
+    public double GetEnemyAttackRange(int id)
     {
         return getEnemyAttackRange(this.game, id);
     }
     
-    bool CanCharacterAttack(int id, const char* attackName)
+    public bool CanCharacterAttack(int id, string attackName)
     {
         return canCharacterAttack(this.game, id, attackName);
     }
         
-    double GetDamage(int id, const char* attackName)
+    public double GetDamage(int id, string attackName)
     {
         return getDamage(this.game, id, attackName);
     }
         
-    double GetChargeTime(int id, const char* attackName)
+    public double GetChargeTime(int id, string attackName)
     {
         return getChargeTime(this.game, id, attackName);
     }
         
-    double GetCharacterHurtTime(int id)
+    public double GetCharacterHurtTime(int id)
     {
         return getCharacterHurtTime(this.game, id);
     }
     
-    bool IsCharacterBusy(int id)
+    public bool IsCharacterBusy(int id)
     {
         return isCharacterBusy(this.game, id);
     }
     
-    int GetCharacterHealth(int id)
+    public int GetCharacterHealth(int id)
     {
         return getCharacterHealth(this.game, id);
     }
     
-    int GetCharacterMaxHealth(int id)
+    public int GetCharacterMaxHealth(int id)
     {
         return getCharacterMaxHealth(this.game, id);
     }
     
-    double GetPlayerDashForce()
+    public double GetPlayerDashForce()
     {
         return getPlayerDashForce(this.game);
     }
     
-    double GetJetPackForce()
+    public double GetJetPackForce()
     {
         return getJetPackForce(this.game);
     }
     
-    double GetJetPackMaxTime()
+    public double GetJetPackMaxTime()
     {
         return getJetPackMaxTime(this.game);
     }
     
-    double GetPlayerLandingTime()
+    public double GetPlayerLandingTime()
     {
         return getPlayerLandingTime(this.game);
     }
     
-    double GetPlayerDashTime()
+    public double GetPlayerDashTime()
     {
         return getPlayerDashTime(this.game);
     }
     
-    double GetCharacterAttackTime(int id, const char* attackName)
+    public double GetCharacterAttackTime(int id, string attackName)
     {
         return getCharacterAttackTime(this.game, id, attackName);
     }
 
-    bool IsPlayerDashing()
+    public bool IsPlayerDashing()
     {
         return isPlayerDashing(this.game);
     }
     
-    bool IsPlayerUsingJetpack()
+    public bool IsPlayerUsingJetpack()
     {
         return isPlayerUsingJetpack(this.game);
     }
     
-    bool CanCharacterMove(int id, const char* movementName)
+    public bool CanCharacterMove(int id, string movementName)
     {
         return canCharacterMove(this.game, id, movementName);
     }
         
-    double GetCharacterCoolDownAttack(int id, const char* attackName)
+    public double GetCharacterCoolDownAttack(int id, string attackName)
     {
         return getCharacterCoolDownAttack(this.game, id, attackName);
     }
         
-    bool IsAValidId(int id)
+    public bool IsAValidId(int id)
     {
         return isAValidId(this.game, id);
     }
     
-    int GetPlayerId()
+    public int GetPlayerId()
     {
         return getPlayerId(this.game);
     }
     
-    void Attack(int id, const char* attackName, int targetId)
+    public void Attack(int id, string attackName, int targetId)
     {
         attack(id, attackName, targetId);
     }
     
-    void Move(int id, const char* movementName)
+    public void Move(int id, string movementName)
     {
         move(this.game, id, movementName);
     }
