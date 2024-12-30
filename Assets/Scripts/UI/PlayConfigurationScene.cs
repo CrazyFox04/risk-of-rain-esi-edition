f67e8988bf1ae5ee731c39b65349b51536839259
Thomas Malfroid g60552 <60552@etu.he2b.be>
2024-12-30 05:13:26 +0100

    Changement scenes

--- Assets/Scripts/UI/PlayConfigurationScene.cs
+++ Assets/Scripts/UI/PlayConfigurationScene.cs
@@ -1,7 +1,17 @@
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayConfiguration : MenuScene
 {
+    public string initializerScene;
+    public string blabla;
+    public void PlayGame()
+    {
+        SceneLoader.LoadSceneIfExists(initializerScene);
+    }
 
+    public void Blabla() {
+        SceneLoader.LoadSceneIfExists(blabla);
+    }
 }
