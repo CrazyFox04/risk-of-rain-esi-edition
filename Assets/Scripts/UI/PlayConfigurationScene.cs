0b24576a060365c5d60db5c8ed5a28569a532931
Thomas Malfroid g60552 <60552@etu.he2b.be>
2024-12-31 13:53:10 +0100

    Player Config scene script

--- Assets/Scripts/UI/PlayConfigurationScene.cs
+++ Assets/Scripts/UI/PlayConfigurationScene.cs
@@ -6,68 +6,46 @@ using UnityEngine.UI;
 public class PlayConfiguration : MenuScene
 {
     public string initializerScene;
-    public Dropdown attack1Dpd;
-    public Dropdown attack2Dpd;
-    public Dropdown attack3Dpd;
+    public Dropdown lightAttackDpd;
+    public Dropdown strongAttackDpd;
 
     public PlayerConfig playerConfig;
 
-    private List<string> allAttacks = new List<string>() {"Attack1", "Attack2", "Attack3", "Attack4", "Attack5"};
+    
+    [SerializeField] private List<string> lightAttacks = new List<string>() {"Light Distance", "Light Short"}; // 2 et 4
+    [SerializeField] private List<string> strongAttacks = new List<string>() {"Strong Distance", "Strong Short"}; // 3 et 5
+
+    private int LIGHT = 0;
+    private int STRONG = 1;
 
     void Start() 
     {
-        allAttacks = new List<string>(attack1Dpd.options.ConvertAll(option => option.text));
-
-        attack1Dpd.onValueChanged.AddListener(delegate { UpdateDropdownAttacks(); });
-        attack2Dpd.onValueChanged.AddListener(delegate { UpdateDropdownAttacks(); });
-        attack3Dpd.onValueChanged.AddListener(delegate { UpdateDropdownAttacks(); });
+        InitializeAttackDropDown(lightAttackDpd, 0);
+        InitializeAttackDropDown(strongAttackDpd, 1);
     }
 
-    void UpdateDropdownAttacks() 
-    {
-        string selectAttack1 = attack1Dpd.options[attack1Dpd.value].text;
-        string selectAttack2 = attack1Dpd.options[attack2Dpd.value].text;
-        string selectAttack3 = attack1Dpd.options[attack3Dpd.value].text;
-
-        List<string> availableAttacks1 = GetAvailableOptions(new List<string> { selectAttack2, selectAttack3 });
-        List<string> availableAttacks2 = GetAvailableOptions(new List<string> { selectAttack1, selectAttack3 });
-        List<string> availableAttacks3 = GetAvailableOptions(new List<string> { selectAttack1, selectAttack2 });
-
-        SetDropdownOptions(attack1Dpd, availableAttacks1, selectAttack1);
-        SetDropdownOptions(attack2Dpd, availableAttacks2, selectAttack2);
-        SetDropdownOptions(attack3Dpd, availableAttacks3, selectAttack3);
-    }
-
-    List<string> GetAvailableOptions(List<string> usedOptions)
-    {
-        return allAttacks.FindAll(option => !usedOptions.Contains(option));
-    }
-
-    void SetDropdownOptions(Dropdown attackDpd, List<string> availableAttacks, string currentAttack)
-    {
-        bool keepCurrentSelection = availableAttacks.Contains(currentAttack);
-
+    void InitializeAttackDropDown(Dropdown attackDpd, int type) {
         attackDpd.ClearOptions();
-        attackDpd.AddOptions(availableAttacks);
 
-        if (keepCurrentSelection)
-        {
-            attackDpd.value = availableAttacks.IndexOf(currentAttack);
-        }
-        else
-        {
-            attackDpd.value = 0;
+        if (type == LIGHT) {
+            attackDpd.AddOptions(lightAttacks);
+            
         }
 
+        if (type == STRONG) {
+            attackDpd.AddOptions(strongAttacks);
+        }
+
+        attackDpd.value = 0;
         attackDpd.RefreshShownValue();
     }
     
     public void PlayGame()
     {
         // save and play
-        playerConfig.attack1 = 0; //attack1Dpd.value;
-        playerConfig.attack2 = 1; //attack2Dpd.value;
-        playerConfig.attack3 = 2; //attack3Dpd.value;
+        playerConfig.attack1 = 0;
+        playerConfig.attack2 = (lightAttackDpd.value == 0) ? 1 : 3;
+        playerConfig.attack3 = (strongAttackDpd.value == 0) ? 2 : 4;
 
         SceneLoader.LoadSceneIfExists(initializerScene);
     }
