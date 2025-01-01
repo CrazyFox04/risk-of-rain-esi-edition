255fe17327c1fdd1a9b00ed228ce43bca3360ad4
Marcel Angielczyk <99039274+Ixarito@users.noreply.github.com>
2025-01-01 16:33:17 +0100

    Chest updaye

--- Assets/Scripts/World/Chest.cs
+++ Assets/Scripts/World/Chest.cs
@@ -49,7 +49,7 @@ public class Chest : MonoBehaviour
     {
         //TODO
         //can open chest
-        if (Input.GetButtonDown("Use") && true && Vector2.Distance(player.position, transform.position) < 0.5 && !gameController.isChestEmpty(row, column, id))
+        if (Input.GetButtonDown("Use") && true && Vector2.Distance(player.position, transform.position) < 1 && !gameController.isChestEmpty(row, column, id))
         {
             Debug.Log("Chest opened : " + gameController.openChest(row, column, id));
             isOpen = true;
