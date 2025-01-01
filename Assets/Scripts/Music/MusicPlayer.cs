e94165c22af206487054f2ab4c09f09abe6432b7
Thomas Malfroid g60552 <60552@etu.he2b.be>
2025-01-01 22:01:14 +0100

    Music ambiance
    - Adding script for music in a scene
    - Adding 2 musics in folders Musics/Menu and /Musics/Game

---
+++ Assets/Scripts/Music/MusicPlayer.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMusicPlayer : MonoBehaviour
+{
+    [Header("Music Path")]
+    public string musicFolderPath;
+
+    private AudioSource audioSrc;
+    private List<AudioClip> musics;
+
+    void Start()
+    {
+        audioSrc = gameObject.AddComponent<AudioSource>();
+        audioSrc.loop = false;
+
+        LoadMusic();
+        PlayRandomTrack();
+    }
+
+    void Update()
+    {
+        // verify if music is finished before playing another
+        if (!audioSrc.isPlaying && musics.Count > 0)
+        {
+            PlayRandomTrack();
+        }
+    }
+
+    private void LoadMusic()
+    {
+        // load all clips from folder to the list
+        musics = new List<AudioClip>(Resources.LoadAll<AudioClip>(musicFolderPath));
+
+        if (musics.Count == 0)
+        {
+            Debug.LogWarning($"No music found in path: {musicFolderPath}");
+        }
+    }
+
+    private void PlayRandomTrack()
+    {
+        if (musics.Count > 0)
+        {
+            AudioClip randomClip = musics[Random.Range(0, musics.Count)];
+            audioSrc.clip = randomClip;
+            audioSrc.Play();
+        }
+    }
+}
